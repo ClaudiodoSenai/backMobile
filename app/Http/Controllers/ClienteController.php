e83c8945ac69f0f7e5340fc3d5e42cfbf05a0004
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2024-04-04 15:31:25 -0300

    04/04

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClienteFormRequest;
+use App\Http\Requests\ClienteUpdateFormRequest;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ClienteController extends Controller
+{
+    
+    public function indexCliente()
+    {
+        $clientes = Cliente::all();
+
+        $clientesComfoto = $clientes->map(function ($cliente) {
+            return [
+                'nome' => $cliente->nome,
+                'telefone' => $cliente->telefone,
+                'endereco' => $cliente->endereco,
+                'email'=> $cliente->email,
+                'password'=>$cliente->password,
+                'foto' => asset('storage/' . $cliente->foto),
+            ];
+        });
+        return response()->json($clientesComfoto);
+    }
+    public function storeCliente(ClienteFormRequest $request)
+    {
+        $clienteData = $request->all();
+
+        if($request->hasFile('foto')){
+            $foto = $request->file('foto');
+            $nomefoto = time(). '.'. $foto->getClientOriginalExtension();
+            $caminhofoto = $foto->storeAs('foto/clientes',$nomefoto, 'public');
+            $clienteData['foto'] = $caminhofoto;
+        }
+        $cliente = cliente::create($clienteData);
+        return response ()-> json (['cliente' => $cliente],201);
+    }
+   
+}
